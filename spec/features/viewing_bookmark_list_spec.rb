3ccd6a34ac8570d8202a68409063b7a912ddb534
Dan Gyi <daniel.gyi@gmail.com>
2019-09-09 13:25:36 +0100

    failing feature test for displaying bookmarks

--- spec/features/viewing_bookmark_list_spec.rb
+++ spec/features/viewing_bookmark_list_spec.rb
@@ -5,10 +5,18 @@ feature 'Viewing Bookmarks' do
     expect(page).to have_content("Bookmark Manager")
   end
 
-  scenario 'visit bookmarks page & view bookmarks' do
+  scenario 'visit bookmarks page' do
     visit '/'
     click_button 'View Bookmarks'
     expect(page).to have_content('Your Bookmarks')
+  end
+
+  scenario 'visit bookmarks page & view bookmarks' do
+    visit '/'
+    click_button 'View Bookmarks'
+    expect(page).to have_content('Google.com')
+    expect(page).to have_content('Netflix.com')
+    expect(page).to have_content('Amazon.com')
   end
 
 end
