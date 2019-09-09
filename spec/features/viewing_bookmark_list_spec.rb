e71801c8e766a48f64c916c51816b54926fa9a47
Dan Gyi <daniel.gyi@gmail.com>
2019-09-09 13:04:09 +0100

    test failing - view bookmarks on /bookmarks

--- spec/features/viewing_bookmark_list_spec.rb
+++ spec/features/viewing_bookmark_list_spec.rb
@@ -1,6 +1,14 @@
 feature 'Viewing Bookmarks' do
+  
   scenario 'visit index page' do
     visit '/'
     expect(page).to have_content("Bookmark Manager")
   end
+
+  scenario 'visit bookmarks page & view bookmarks' do
+    visit '/'
+    click_button 'View Bookmarks'
+    expect(page).to have_content('Bookmarks List')
+  end
+
 end
