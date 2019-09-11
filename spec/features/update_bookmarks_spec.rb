64533fa096d9d9761db0a8655e595fb0b9b7f9ea
Dan Gyi <daniel.gyi@gmail.com>
2019-09-11 12:16:17 +0100

    update bookmark feature complete, tests passing

--- spec/features/update_bookmarks_spec.rb
+++ spec/features/update_bookmarks_spec.rb
@@ -2,16 +2,16 @@ require_relative 'web_helpers'
 
 feature 'Update Bookmark' do
   scenario "Click update bookmark and change url/title" do
+    delete_db_rows
+    bookmark = Bookmarks.add(url: 'https://www.test.com', title: 'Test Bookmark')
     visit '/'
-    first('bookmark').click_button 'Edit'
-    expect(current_path).to eq "/bookmarks/#{bookmark.id}/edit"
-    fill_in 'website', with: "http://www.bbc.co.uk/sport/football"
-    fill_in 'bookmark_title', with: "BBC Football"
+    click_button 'Edit'
+    expect(current_path).to eq "/#{bookmark.id}/edit"
+    fill_in 'url', with: "http://www.bbc.co.uk/sport/football"
+    fill_in 'title', with: "BBC Football"
     click_button "Submit"
-    expect(current_path).to eq '/bookmarks'
-    expect(page).not_to have_content("BBC Sport")
-    expect(page).not_to have_link("http://www.bbc.co.uk/sport")
+    expect(current_path).to eq '/'
+    expect(page).not_to have_content("Test Bookmark")
     expect(page).to have_content("BBC Football")
-    expect(page).to have_link("http://www.bbc.co.uk/sport/football")
   end
 end
