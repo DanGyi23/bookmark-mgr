155d23a1b3780dbf5ef6f856e36d5b16239556f4
Dan Gyi <daniel.gyi@gmail.com>
2019-09-11 10:56:13 +0100

    update bookmark feature test failing

--- spec/features/update_bookmarks_spec.rb
+++ spec/features/update_bookmarks_spec.rb
@@ -3,10 +3,12 @@ require_relative 'web_helpers'
 feature 'Update Bookmark' do
   scenario "Click update bookmark and change url/title" do
     visit '/'
-    find("[id=update]").find("[name=#{Bookmarks.all.last.id}]").click
+    find("[value=#{Bookmarks.all.last.id}]").click_button 'Edit'
+    expect(current_path).to eq "/bookmarks/#{bookmark.id}/edit"
     fill_in 'website', with: "http://www.bbc.co.uk/sport/football"
     fill_in 'bookmark_title', with: "BBC Football"
     click_button "Submit"
+    expect(current_path).to eq '/bookmarks'
     expect(page).not_to have_content("BBC Sport")
     expect(page).not_to have_link("http://www.bbc.co.uk/sport")
     expect(page).to have_content("BBC Football")
